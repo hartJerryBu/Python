3c9cd55b15929d6d53ca2215d30120a84e86c010
hartJerryBu <martyhart@outlook.cz>
2026-04-09 14:57:32 +0200

    Add files via upload

---
+++ skript1.py
@@ -0,0 +1,31 @@
+print('Ahoj Martin')
+print('doprdele prace')
+print( ' Praha, Brno, Olomouc ' )
+print( ' Hart je psisko ')
+print( ' Hart je psisko ')
+print( ' Je to neuvěřitelne ')
+print( ' musíš se to Marťo naučit - nejsi hňup ')
+print( 'jak to jde ')
+print( ' prosím vás jak to funguje')
+print( 'ahoj Marto ')
+print( 'konečně se do toho obuj ')
+
+print("hjjjh")
+print("gghh")
+
+print("jkhgyfthdrdsgsg")
+print("gghh")
+
+print("hjjjh")
+print("gghh")
+
+print("jkhgyfthdrdsgsg")
+print("gghh")
+
+
+
+
+
+
+
+
